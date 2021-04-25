79e23785f20ef3574bdeafb94d24d26f1d77584d
gustavofcomin <gustavofcomin@gmail.com>
2021-04-25 20:22:46 -0300

    style menu

--- Categoria/Listar/listar_Categoria.php
+++ Categoria/Listar/listar_Categoria.php
@@ -6,7 +6,7 @@ include('../../conexao.php');
 
 <head>
 	<title>Listar Categoria</title>
-			<link rel="stylesheet" type="text/css" href="../Styles/site.css">
+	<link rel="stylesheet" type="text/css" href="../Styles/site.css">
 </head>
 
 <body>
