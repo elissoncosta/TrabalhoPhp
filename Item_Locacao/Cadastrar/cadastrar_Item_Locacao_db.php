0e2e50e1f6c35b9c565fec378d66d3cb888c9a7c
gustavofcomin <gustavofcomin@gmail.com>
2021-04-25 16:00:58 -0300

    a

--- Item_Locacao/Cadastrar/cadastrar_Item_Locacao_db.php
+++ Item_Locacao/Cadastrar/cadastrar_Item_Locacao_db.php
@@ -6,7 +6,7 @@ $IdLocacao = $_POST['IdLocacao'];
 $IdTitulo  = $_POST['IdTitulo'];
 
 $sql = "INSERT INTO ITEM_LOCACAO 
-        VALUES (null, '{$Sequence}', '{$IdLocacao}', '{$IdTitulo}'";
+        VALUES (null, '{$Sequence}', '{$IdLocacao}', '{$IdTitulo}')";
 
 $query = mysqli_query($conexao, $sql);
 if ($query) {
