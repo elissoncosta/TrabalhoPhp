efbfc9e5cb20f004bd9fd8a268a5f61cf068b0cd
elissoncosta <lsson@hotmail.it>
2021-04-26 00:13:31 -0300

    alterações locação

--- Locacao/Excluir/excluir_locacoes.php
+++ Locacao/Excluir/excluir_locacoes.php
@@ -4,12 +4,15 @@
 <head>
 	<title>Excluir Locação</title>
 	<link rel="stylesheet" type="text/css" href="../../Styles/Excluir.css">
+	<link rel="stylesheet" type="text/css" href="../../Styles/Listar.css">
 </head>
 
 <body>
-	<?php
-	include('../../menu.php');
-	?>
+	<nav id="menu-h">
+		<?php
+		include('../../menu.php');
+		?>
+	</nav>
 	<div class="wrapper">
 		<div class="campos">
 			<form action="excluir_locacoes_db.php" method="post">
--- Locacao/Listar/listar_locacoes.php
+++ Locacao/Listar/listar_locacoes.php
@@ -7,12 +7,24 @@ include('../../conexao.php');
 <head>
 	<title>Listar Cliente Locações</title>
 	<link rel="stylesheet" type="text/css" href="../Styles/site.css">
+	<link rel="stylesheet" type="text/css" href="../../Styles/Listar.css">
 </head>
 
 <body>
-	<?php
-	include('../../menu.php');
-	?>
+	<nav id="menu-h">
+			<?php
+			include('../../menu.php');
+			?>
+	</nav>
+
+	<div id="atalho"></div>
+
+	<div id="filtro" class="filtro">
+		<button type="button"><a href="../Cadastrar/cadastrar_locacoes.php">Cadastrar Novo Locação</a></button>
+		<input class="button_pesquisar" type="search" class="busca">
+		<button type="button">Buscar</button>		
+	</div>
+
 	<?php
 	$ok   = @$_GET['ok'];
 	$erro = @$_GET['erro'];
@@ -29,61 +41,69 @@ include('../../conexao.php');
 		echo "<p class=\"erro\">Locação não excluída! MySQL erro: {$msg}</p>";
 	}
 	?>
-	<br><br><a href="../Cadastrar/cadastrar_locacoes.php">Cadastrar</a><br><br>
-	<table>
-		<thead>
-			<tr>
+	<div id="lista">
+		<table>
+			<thead class="header_list">
+				<tr>
 				<th>Código</th>
 				<th>Código Cliente</th>
 				<th>Nome Cliente</th>
 				<th>Valor</th>
 				<th>Data da Locação</th>
 				<th>Data da Entrega</th>
-				<th>Ações</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			$sql = "SELECT L.id_locacao AS Id,
-						   L.cliente_id,
-		                   C.nome,
-		                   L.valor,
-		                   L.data_locacao,
-		                   L.data_entrega 
-	                  FROM LOCACAO L
-	                  JOIN CLIENTE C ON (C.ID_CLIENTE = L.CLIENTE_ID)
-					  ORDER BY L.id_locacao";
-
-			$query = mysqli_query($conexao, $sql);
-			if (!$query) {
-			?>
-				<tr>
-					<td colspan="3"><?php echo 'MySQL Erro: ' . mysqli_error($conexao); ?></td>
+				<th colspan="2">Ações</th>
 				</tr>
+			</thead>
+			<tbody>
 				<?php
-			} else {
-				while ($item = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+				$sql = "SELECT L.id_locacao AS Id,
+							L.cliente_id,
+							C.nome,
+							L.valor,
+							L.data_locacao,
+							L.data_entrega 
+						FROM LOCACAO L
+						JOIN CLIENTE C ON (C.ID_CLIENTE = L.CLIENTE_ID)
+						ORDER BY L.id_locacao";
+
+				$query = mysqli_query($conexao, $sql);
+				if (!$query) {
 				?>
 					<tr>
-						<td><?php echo $item['Id']; ?></td>
-						<td><?php echo $item['cliente_id']; ?></td>
-						<td><?php echo $item['nome']; ?></td>
-						<td><?php echo $item['valor']; ?></td>
-						<td><?php echo $item['data_locacao']; ?></td>
-						<td><?php echo $item['data_entrega']; ?></td>
-						<td>
-							<a href="../Alterar/alterar_locacoes.php?Id=<?php echo $item['Id']; ?>">Alterar</a>
-							<a href="../Excluir/excluir_locacoes.php?Id=<?php echo $item['Id']; ?>">Excluir</a>
-						</td>
+						<td colspan="3"><?php echo 'MySQL Erro: ' . mysqli_error($conexao); ?></td>
 					</tr>
-			<?php
+					<?php
+				} else {
+					while ($item = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+					?>
+						<tr>
+							<td><?php echo $item['Id']; ?></td>
+							<td><?php echo $item['cliente_id']; ?></td>
+							<td><?php echo $item['nome']; ?></td>
+							<td><?php echo $item['valor']; ?></td>
+							<td><?php echo $item['data_locacao']; ?></td>
+							<td><?php echo $item['data_entrega']; ?></td>
+							<td>
+								<button class="btn" type="button">
+									<a href="../Alterar/alterar_locacoes.php?Id=<?php echo $item['Id']; ?>">Alterar</a>
+								</button>
+							</td>
+							<td>
+								<button class="btn" type="button">
+									<a href="../Excluir/excluir_locacoes.php?Id=<?php echo $item['Id']; ?>">Excluir</a>
+								</button>								
+							</td>
+
+						</tr>
+				<?php
+					}
 				}
-			}
-			?>
-		</tbody>
-	</table>
-	Exitem <?php echo mysqli_num_rows($query); ?> Itens
-</body>
+				?>
+			</tbody>
+		</table>
+		Exitem <?php echo mysqli_num_rows($query); ?> Itens
+	</div>
+	</body>
 
 </html>
 <?php
